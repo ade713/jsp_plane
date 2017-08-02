b88f31b1b63046bc52eef979b68d3cfb8aabbb32
Ade Farquhar <adefarquhar@gmail.com>
2017-08-02 13:26:17 -0700

    add plane sprite to plane class

--- entry.js
+++ entry.js
@@ -1,3 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
+import Game from './lib/game';
+import Plane from './lib/plane';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('gameCanvas');
+  const ctx = canvas.getContext('2d');
+
+  let game = new Game(ctx);
+  let p = new Plane(ctx);
+
+  let stage;
+
+  const animate = () => {
+    draw();
+    console.log('time');
+    requestAnimationFrame(animate);
+  };
+
+  function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillStyle = '#fff';
+    ctx.fillText(`Score: ${game.score}`, 10, 30);
+  }
+
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    p.planeImg();
+    
+    drawScore();
+  }
+
+  animate();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,7 @@
-var path = require('path');
-
 module.exports = {
   entry: './entry.js',
   output: {
     filename: './bundle.js',
   },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '*']
-  }
+  devtool: 'source-map'
 };
