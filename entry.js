64c9e622f078cd9f619172d51df862a22621fa69
Ade Farquhar <adefarquhar@gmail.com>
2017-08-03 07:18:54 -0700

    add pause button/functionality

--- entry.js
+++ entry.js
@@ -8,44 +8,68 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let game = new Game(ctx);
   let plane = new Plane(ctx);
-  let proj = new Projectile(ctx);
+  let proj = new Projectile(ctx, plane.coords.fieldX);
+  let paused;
 
   let keyPress;
   window.addEventListener("keydown", event => {
     keyPress = event.keyCode;
   });
   window.addEventListener("keyup", event => {
-    keyPress = false;
+    if (keyPress === 32) {
+      keyPress;
+    } else {
+      keyPress = false;
+    }
   });
 
-  const animate = () => {
-    display();
-    requestAnimationFrame(animate);
-  };
-
-  function drawScore() {
+  function drawScore(score) {
     ctx.font = '20px Arial';
     ctx.fillStyle = '#fff';
-    ctx.fillText(`Score: ${game.score}`, 10, 30);
+    ctx.fillText(`Score: ${score}`, 10, 30);
   }
 
   function display() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let dx = 5;
-    let speed = -15;
+    let speed = -5;
 
     if (keyPress === 39) {
       plane.move(dx);
     } else if (keyPress === 37) {
       plane.move(-dx);
     } else if (keyPress === 32) {
+      proj.projectileImg();
       proj.shoot(speed);
     }
-    plane.planeImg();
-    proj.projectileImg();
 
-    drawScore();
+    plane.planeImg();
+
+    console.log(game.score());
+    drawScore(game.score());
   }
+
+  const animate = () => {
+    display();
+
+    if (!paused) {
+      requestAnimationFrame(animate);
+    }
+  };
+
+  paused = false;
+
+  function togglePause() {
+    paused = !paused;
+    if (!paused) {
+      animate();
+    }
+    document.getElementById("pauseButton").value = paused ? "unpause" : "pause";
+  }
+
+  document.addEventListener('click', () => {
+    togglePause();
+  });
 
   animate();
 });
